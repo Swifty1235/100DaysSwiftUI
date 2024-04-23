830d094c1aee9f5bddcaeabee9e78209d3629405
Pedro Romero <104468694+Swifty1235@users.noreply.github.com>
2024-04-23 19:18:57 -0400

    Add files via upload

---
+++ 2ndDaySwiftIUi.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let goodDogs = true
+
+print(goodDogs)
+
+var isAuthenticated = false
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+
+var goodTogo = false
+goodTogo.toggle()
+
+let people = "hey you are"
+let name = "pedro"
+let num = 5
+
+let message = "hi im \(name)"
+print (message)
+
+print (people + " " + name)
+print ("hey you are \(name) and the second message is \(people)")
