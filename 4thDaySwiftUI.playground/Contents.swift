932ce7de5119d461dc4b0d8677839945c23eb031
Pedro Romero <104468694+Swifty1235@users.noreply.github.com>
2024-04-25 18:44:17 -0400

    Add files via upload

---
+++ 4thDaySwiftUI.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+/*
+var greeting = "Hello, playground"
+
+//type annotations
+
+let name: String = "Pedro"
+
+
+print(name)
+
+var genreMusic: [String] = ["pop" , "rap" , "jazz"]
+
+let music: [String] = [String]()
+
+var game = [String]()
+
+enum time {
+    case light, dark, defaultt
+}
+
+var style = time.light
+
+print(style)
+
+
+// say with constants, you can create a empty variable or array, set, whatever....
+//then you can change or add to it once, but afterwards no, so temp variable in a sense
+
+let timeOfday : String          //have to use : instead of =
+timeOfday = "Sunset"
+print(timeOfday)
+ */
+
+//CheckPoint 2 Assignment
+
+let animeList = ["One Piece" , "JJK" , "My Hero Academia" , "Jobless Recarnation" , "One Punch Man"]
+
+print("Number of items is:  \(animeList.count)")
+
+      let tvShows : Set = (["Witcher" , "Uncut Gems" , "Black Mirror"])
+                         print (tvShows.count)
+print(tvShows)
