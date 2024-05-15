7d3ada000c7be9509206d06cc3df19a0b295367c
Pedro Romero <104468694+Swifty1235@users.noreply.github.com>
2024-05-15 19:28:43 -0400

    Add files via upload

---
+++ Day 6 FizzBizz Checkpoint.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+for i in 1...100{
+    if i.isMultiple(of: 3) && !i.isMultiple(of: 5){
+        print("Fizz")
+    }
+    else if i.isMultiple(of: 5) && !i.isMultiple(of: 3){
+        print("Buzz")
+    }
+    else if i.isMultiple(of: 3) && i.isMultiple(of: 5){
+        print("FizzBuzz")
+    }
+    else
+    {
+        print(i)
+
+    }
+}
