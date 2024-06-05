f6d5d21c70c6f09e8a754afee3e372c3d7fb3a59
Pedro Romero <104468694+Swifty1235@users.noreply.github.com>
2024-06-04 20:46:16 -0400

    Add files via upload

---
+++ 14thDayofSwiftUI.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+/*
+var myVar: Int? = 3
+
+if let unwrapped = myVar {
+    // Run if myVar has a value inside
+}
+guard let unwrapped = myVar else {
+    return
+    // Run if myVar doesn't have a value inside
+}
+
+*/
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+
+    print("\(number) x \(number) is \(number * number)")
+}
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity", default: "N/A"]
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+
+//?? default value if value is missing
+
+
+//?.uppercased , if no nil then run this. otherwise don't
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
